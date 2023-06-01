cf335183400c3cc0f5e1e9a8da92da6536c3a520
AidenParkerr <aidenparker1998@live.co.uk>
2023-06-01 20:24:10 +0100

    Improve backup manager script
    This commit introduces several improvements and fixes to the backup manager script:
    
    - Added error handling for backup and compression processes. The script now catches exceptions thrown by subprocess.run and logs them, also sending a notification via Telegram.
    - Included a timeout for the backup and compression processes to prevent the script from hanging indefinitely if these processes take too long.
    - Added a cleanup function to delete incomplete backup files in case of errors.
    - Introduced a lock file mechanism to prevent multiple instances of the script from running simultaneously.
    - Fixed a bug where Telegram bot token and chat ID were not being correctly loaded from the config file.
    - Improved logging, including more detailed log messages and logging to both a file and the console.
    - Implemented command-line argument parsing to allow backup file location, config file location, device name, and timeout to be specified when running the script.
    - Added signal handling to allow the script to be safely interrupted with SIGINT (Ctrl+C) or SIGTERM.

--- README.md
+++ README.md
@@ -1,37 +1,42 @@
 # Backup Manager
 
-The Backup Manager is a Python script that automates the process of backing up your Raspberry Pi's SD card. This script also utilizes the Telegram API to send notifications about the backup process.
+The Backup Manager is a Python script that automates the process of backing up a device (such as a Raspberry Pi's SD card). This script utilizes the Telegram API to send notifications about the backup process, making it easier to monitor its progress and status.## Features
 
 ## Features
 
-- Automatically backs up the entire SD card.
-- Compresses the backup image using `gzip`.
-- Logs the backup process, logging can be viewed in `backup_manager.log`.
+- Automatically backs up a specified device.
+- Compresses the backup image using gzip.
+- Logs the backup process, with logs stored in backup_manager.log.
 - Prevents simultaneous runs of the script by using a lock file mechanism.
 - Sends notifications about the backup process via Telegram.
+- Uses a timeout for the backup and compression processes to prevent hanging indefinitely.
+- Cleans up and deletes incomplete backup files in case of errors.
+- Allows for the specification of backup file location, config file location, device name, and timeout when running the script.
 
 ## Requirements
 
-The script requires the following Python packages:
-- `configparser`
-- `argsparse`
-- `subprocess`
-- `requests`
-- `logging`
-- `os`
+The script requires Python 3 and the following Python packages:
+
+- configparser
+- argparse
+- subprocess
+- requests
+- logging
+- os
+- fcntl
+- signal
+- time
 
 These packages are included in the standard Python 3 library.
 
 ## Configuration
 
-You need to create a `config.ini` file in the same directory as the script. This file should contain your Telegram bot token and chat ID in the following format:
-
+You need to create a `config.ini` file in the same directory as the script or specify the path to it when running the script. This file should contain your Telegram bot token and chat ID in the following format:
 ```
 [Telegram]
 bot_token = "your_bot_token"
 chat_id = "your_chat_id"
 ```
-
 
 Please note that the `config.ini` file should not be shared or uploaded to public repositories as it contains sensitive data.
 
@@ -40,13 +45,20 @@ Please note that the `config.ini` file should not be shared or uploaded to publi
 You can run the script with the following command:
 
 ```
-python3 backup_manager.py
+python3 backup_manager.py --backup_file <backup file path> --config_file <config file path> --device_name <device name> --timeout <timeout in seconds>
 ```
 
 The default device name for notifications is "PlexServer". If you want to use a different device name, you can change the argument in the `run_backup` function at the bottom of the script.
 
 
+The arguments are as follows:
 
+- `backup_file`: The path to the location where the backup file will be stored.
+- `config_file`: The path to the configuration file (config.ini).
+- `device_name`: The name of the device being backed up.
+- `timeout`: The timeout for the backup and compression processes in seconds.
+
+If any arguments are not provided, the script will use default values (excluding `backup_file` and `config_file`). The default device name is "device1", and the default timeout is 3600 seconds (1 hour).
 
 
 
--- backup_manager.py
+++ backup_manager.py
@@ -4,43 +4,49 @@ import subprocess
 import requests
 import logging
 import os
-
+import fcntl
+import signal
+import time
 
 class BackupManager:
     def __init__(self, backup_file) -> None:
+        if not os.path.isdir(os.path.dirname(backup_file)):
+            raise ValueError("Invalid backup file path.")
         self.backup_file = backup_file
         self.backup_command = f"sudo dd if=/dev/mmcblk0 of={self.backup_file} bs=1M"
+        self.lock_file = "/tmp/backup_manager.lock"
         self._init_logger()
         self.load_telegram_config()
-        self.lock_file = "/tmp/backup_manager.lock"
+        signal.signal(signal.SIGINT, self._signal_handler)
+        signal.signal(signal.SIGTERM, self._signal_handler)
 
     def _init_logger(self) -> None:
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
 
-        log_path = os.path.join(
-            os.path.dirname(
-                os.path.abspath(__file__)),
-            'backup_manager.log')
+        log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'backup_manager.log')
         file_handler = logging.FileHandler(log_path)
         console_handler = logging.StreamHandler()
 
         file_handler.setLevel(logging.DEBUG)
         console_handler.setLevel(logging.DEBUG)
 
-        file_format = logging.Formatter(
-                '%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-        console_format = logging.Formatter(
-            '%(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+        console_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
         file_handler.setFormatter(file_format)
         console_handler.setFormatter(console_format)
 
         self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
 
-    def load_telegram_config(self):
-        self.logger.info("Loading Telegram bot token and chat ID...")
-        
+    def _signal_handler(self, signum, frame) -> None:
+        self.logger.info("Received signal to stop backup process. Exiting...")
+        self.send_notification("*Backup process stopped.*")
+        self.cleanup(True)
+        exit(0)
+
+
+    def _read_config(self):
         config = configparser.ConfigParser()
         config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.ini")
         config.read(config_path)
@@ -48,63 +54,67 @@ class BackupManager:
         self.bot_token = config['Telegram']['bot_token'].strip('"')
         self.chat_id = config['Telegram']['chat_id'].strip('"')
 
+        
+        
+    def load_telegram_config(self):
+        self.logger.info("Loading Telegram bot token and chat ID...")
+        self._read_config()
+        
         if self.bot_token is None or self.chat_id is None:
-            self.logger.critical(
-                "Telegram bot token or chat ID not found. Exiting...")
-            exit(1)
+            raise ValueError("Telegram bot token or chat ID not found.")
+        self.logger.critical("Telegram bot token or chat ID not found. Exiting...")
+
         self.logger.info("Telegram bot token and chat ID loaded.")
 
     def send_notification(self, body: str) -> None:
         try:
+            self.logger.info(f"Sending Notification to Telegram: '{body}'")
             tg_api_link = f"https://api.telegram.org/bot{self.bot_token}/sendMessage?chat_id={self.chat_id}&parse_mode=Markdown&text={body}"
-            self.logger.info(f"Sending HTTP request to Telegram API...")
             response = requests.get(tg_api_link)
             if response.status_code == 200:
                 self.logger.info("Notification sent to Telegram.")
             else:
-                self.logger.error(
-                    f"Failed to send notification to Telegram, status code: {response.status_code}")
-        except Exception as e:
+                self.logger.error(f"Failed to send notification to Telegram, status code: {response.status_code}")
+        except requests.exceptions.RequestException as e:
             self.logger.error("Failed to send notification to Telegram", exc_info=True)
 
-    def run_backup(self, device_name: str = "device1"):
-        if os.path.exists(self.lock_file):
-            self.logger.error("An Instance of `backup_manager` is already running. Exiting...")
-            return
+    def execute_backup(self):
+        """Executes the backup command with error handling."""
+        try:
+            self.logger.info("Running backup command...")
+            self.send_notification("*Backup process started.*")
+            
+            start = time.time()
+            subprocess.run(self.backup_command, shell=True, stderr=subprocess.STDOUT, timeout=3600)
+            end = time.time()
+            
+            self.logger.info("Backup process completed. Time taken: %.2f minutes", end-start / 60)
+            self.send_notification(f"*Backup process completed. Time taken: %.2f minutes*", end-start / 60)
+            return True
+        except subprocess.TimeoutExpired:
+            self.logger.error("Backup process timed out.")
+            self.send_notification("*ERROR*: Backup process timed out.") # add optional argument to determine severity of error. i.e. critical, warning, info etc.
+            return False
+        except Exception as e:
+            self.handle_error(e)
+            return False
 
-        with open(self.lock_file, 'w') as file:
-            file.write("lock")
-
-        try:    
-            markdown_message = f"*Backup Started for _{device_name}_ ...*"
-            self.logger.info(f"Backup Started for {device_name}...")
-            self.send_notification(markdown_message)
-
-            error_thrown = False
-            try:
-                self.logger.info("Running backup command...")
-                self.execute_backup_and_zip()
-                self.send_notification("*Backup and compression finished successfully.*")
-            except KeyboardInterrupt:
-                self.logger.error("Backup process interrupted by keyboard.")
-                self.send_notification("*Backup process interrupted by keyboard.*")
-                error_thrown = True
-            except Exception as e: 
-                self.handle_error(e)
-                error_thrown = True
-            finally:
-                self.cleanup(error_thrown)
-        finally:
-            os.remove(self.lock_file)
-
-
-    def execute_backup_and_zip(self):
-        subprocess.check_output(self.backup_command, shell=True, stderr=subprocess.STDOUT)
-        self.logger.info("Backup process completed. Attempting to gzip the backup image...")
-        self.send_notification("Backup process completed. Attempting to gzip the backup image...")
+    def execute_gzip(self):
+        """Executes the gzip command with error handling."""
         gzip_command = f"gzip -c {self.backup_file}"
-        subprocess.check_output(gzip_command, shell=True, stderr=subprocess.STDOUT)
-        self.logger.info("Backup image compressed successfully. Sending notification to Telegram...")
+        try:
+            self.logger.info("Running gzip command...")
+            subprocess.run(gzip_command, shell=True, stderr=subprocess.STDOUT, timeout=3600)
+            self.logger.info("Compression process completed.")
+            self.send_notification("*Compression process completed.*")
+            return True
+        except subprocess.TimeoutExpired:
+            self.logger.error("Compression process timed out.")
+            self.send_notification("*Compression process timed out.*")
+            return False
+        except Exception as e:
+            self.handle_error(e)
+            return False
 
     def handle_error(self, e):
         if isinstance(e, subprocess.CalledProcessError):
@@ -115,20 +125,66 @@ class BackupManager:
             self.logger.error(f"Unexpected error: {e}")
             self.send_notification(f"Backup failed due to an unexpected error: {e}")
 
-
     def cleanup(self, error_thrown: bool):
-        if error_thrown and os.path.exists(self.backup_file):
-            try:
-                os.remove(self.backup_file)
-                self.logger.info("Incomplete backup file deleted.")
-            except Exception as e:
-                self.logger.error(f"Failed to delete incomplete backup file. Error: {e}", exc_info=True)
-        
         if error_thrown:
-            self.send_notification("*Incomplete backup process cleaned up.*")
+            if os.path.exists(self.backup_file):
+                try:
+                    self.logger.info("Deleting incomplete backup file...")
+                    os.remove(self.backup_file)
+                    self.logger.info("Incomplete backup file deleted.")
+                except Exception as e:
+                    self.logger.error(f"Failed to delete incomplete backup file. Error: {e}", exc_info=True)
+            
+            self.send_notification("*Cleaning Done.*")
 
 
 
+    def run(self, device_name: str = "device1"):       
+        # Check if another instance of this script is already running
+        try:
+            lock_file_descriptor = os.open(self.lock_file, os.O_CREAT | os.O_TRUNC | os.O_WRONLY)
+        except OSError as e:
+            self.logger.error(f"Could not open or create lock file: {e}. Exiting...")
+            exit(1)
+        
+        # Try to acquire an exclusive lock on the lock file
+        try:
+            fcntl.lockf(lock_file_descriptor, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        except IOError as e:
+            self.logger.error(f"An Instance of this script is already running: {e}. Exiting...")
+            exit(1)
+        
+        try:    
+            self.send_notification(f"*Starting Backup for device: '{device_name}'...*")
+            # Execute backup and compression commands sequentially.
+            if not self.execute_backup():
+                # If backup fails, delete the incomplete backup file and exit.
+                self.cleanup(error_thrown=True)
+                return
+            
+            if not self.execute_gzip():
+                # If compression fails, delete the incomplete backup file and exit.
+                self.cleanup(error_thrown=True)
+                return
+
+            # Backup and compression finished successfully.
+            self.logger.info("Backup and compression finished successfully.")
+            self.send_notification("*Backup and compression finished successfully.*")
+        except KeyboardInterrupt:
+            # Catch keyboard interrupt (Ctrl+C) and exit.
+            self.logger.error("Backup process interrupted by keyboard.")
+            self.cleanup(error_thrown=True)
+        except Exception as e: 
+            # Catch any other exceptions that might have been missed.
+            self.handle_error(e)
+            self.cleanup(error_thrown=True)
+        finally:
+            # Release the lock on the lock file and delete it.
+            self.logger.info("Removing lock file...")
+            os.close(lock_file_descriptor)
+            os.remove(self.lock_file)
+            self.logger.info("Lock file removed.")
+            
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Backup Manager")
     parser.add_argument("backup_file", help="Backup image location")
@@ -136,4 +192,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     backup_manager = BackupManager(args.backup_file)
-    backup_manager.run_backup("PlexServer")
+    backup_manager.run("PlexServer")
